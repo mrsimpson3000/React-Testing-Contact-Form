2224f206bb12aaa441122ea051b0f4a2d120bb70
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-09 16:05:32 -0400

    Starting my first tests after writing them out on paper.

---
+++ src/components/ContactForm.test.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ContactForm from "./ContactForm";
+
+test("inputs are visible", () => {
+  // ARRANGE - set up the testing environment
+  // ACT
+  // ASSERT
+});

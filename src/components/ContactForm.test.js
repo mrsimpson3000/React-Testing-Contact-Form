408b877a4780a34dc94d6ae618211bdd38db312f
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-09 18:10:21 -0400

    Test is failing but not for the reasons I was hoping it would. Will continue to try.

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,9 +1,35 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, fireEvent } from "@testing-library/react";
 import ContactForm from "./ContactForm";
 
 test("inputs are visible", () => {
   // ARRANGE - set up the testing environment
+  const { getByText } = render(<ContactForm />);
+
+  // ACT -no action
+
+  // ASSERT
+  getByText(/first name/i);
+  getByText(/last name/i);
+  getByText(/email/i);
+  getByText(/message/i);
+});
+
+test("Submit filled out form", () => {
+  const { getByText, getByTestId } = render(<ContactForm />);
+  // ARRANGE -set up the testing environment
+  const fnameInput = getByText(/first name/i);
+  const lnameInput = getByText(/last name/i);
+  const emailInput = getByText(/email/i);
+  const messageInput = getByText(/message/i);
+
   // ACT
+  fireEvent.change(fnameInput, { target: { value: "Chadwick" } });
+  fireEvent.change(lnameInput, { target: { value: "Simpson" } });
+  fireEvent.change(emailInput, { target: { value: "mrsimpson3000@gmail" } });
+  fireEvent.change(messageInput, {
+    target: { value: "I am here and you are not here." },
+  });
+
   // ASSERT
 });
